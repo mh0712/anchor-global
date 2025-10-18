a175dcb5fbce99273c3a730895ffd5720278867d
mh0712 <marchajjar05@gmail.com>
2025-10-18 13:30:31 +0300

    removed useless mouse svg from hero section

--- src/components/ui/hero-section.tsx
+++ src/components/ui/hero-section.tsx
@@ -123,14 +123,6 @@ export function HeroSection() {
         </div>
       </div>
       
-      {/* Scroll Indicator */}
-      <div 
-        className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce cursor-pointer" 
-        onClick={() => smoothScroll('#about')}>
-        <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center hover:border-[rgb(252,251,248)] transition-colors duration-300">
-          <div className="w-1 h-2 bg-white/50 rounded-full mt-2"></div>
-        </div>
-      </div>
     </section>
   );
 }
